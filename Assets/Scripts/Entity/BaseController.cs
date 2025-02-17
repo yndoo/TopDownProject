83f0145aa53c58c0b3cb8f2ced87a7193a6ebe24
yndoo <isabelle2k@naver.com>
2025-02-17 16:03:26 +0900

    [ Feat ] PlayerController 추가 - 플레이어 이동, 방향 구현

---
+++ Assets/Scripts/Entity/BaseController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody;
+
+    //SerializeField 로 인스펙터 창에 공개
+    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] private Transform weaponPivot;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } } // 이동 방향
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection {  get { return lookDirection; } } // 바라보는 방향
+
+    private Vector2 knockback = Vector2.zero; // 넉백의 방향
+    private float knockbackDuration = 0.0f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        HandleAction();
+        Rotate(lookDirection);
+    }
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+        if(knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+    }
+    /// <summary>
+    /// 입력 처리, 이동에 필요한 데이터 처리 할거임
+    /// </summary>
+    protected virtual void HandleAction()
+    {
+
+    }
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * 5;
+        if(knockbackDuration > 0.0f) // 넉백 duration이 남아있다면 아직 적용해야 함
+        {
+            // 넉백을 적용해야한다면 기존 이동방향의 힘을 줄여주고 넉백의 힘을 넣어주겠다. 
+            direction *= 0.2f;
+            direction += knockback;
+        }
+
+        _rigidbody.velocity = direction;
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        // 아크탄젠트로 x와 y값 이용해서 사이 각도 구함 -> 라디안값으로 나와서 디그리로 바꿀거임, Rad2Deg는 3.14를 180으로 만드는 숫자겠지?
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f; // 사이각도가 90도를 넘었다는 건 사분면 중 왼쪽(2,3사분면)이라는 뜻
+
+        characterRenderer.flipX = isLeft; 
+
+        // 무기도 돌려줄거임 
+        if (weaponPivot != null)
+        {
+            weaponPivot.rotation = Quaternion.Euler(0f, 0f, rotZ);
+        }
+    }
+
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        // A벡터 - B벡터 => B가 A를 바라보는 벡터가 나옴 => 방향과 거리 구할 수 있음. 여기서는 normalize로 방향만 가져옴. 그리고 마이너스!! 넉백이니까.
+        knockback = -(other.position - transform.position).normalized * power;
+    }
+}
---
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseController
+{
+    private Camera camera;
+
+    protected override void Start()
+    {
+        base.Start();
+        camera = Camera.main;
+    }
+
+    protected override void HandleAction()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"); // ProjectSettings > InputManager에 정의된 축들 사용 
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition); // 화면상 좌표를 우리가 원하는 월드좌표로 변환
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if(lookDirection.magnitude/*벡터의 크기*/ < 0.9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
+}
